59d68830f6de066b0bc0ee0456adbcaecc3bed0d
Nicco Kunzmann <niccokunzmann@rambler.ru>
2018-11-22 00:35:53 +0100

    click to open workspace

--- bot/workspace.js
+++ bot/workspace.js
@@ -1,4 +1,20 @@
+/*
+ * Load the workspace.
+ */
 
+(function () {
+  var openClose = document.createElement("a");
+  openClose.id = "openClose";
+  openClose.innerText = "Bot";
+  document.body.appendChild(openClose);
+  
+  var workspaceElement = document.createElement("div");
+  workspaceElement.id = "blocklyWorkspace";
+  document.body.appendChild(workspaceElement);
 
-
+  openClose.onclick = function () {
+    workspaceElement.classList.toggle("hidden");
+    openClose.classList.toggle("selected");
+  }
+})();
 
--- load-bot.js
+++ load-bot.js
@@ -12,6 +12,19 @@
     console.log("loadScript", name);
   };
 
+  function loadStyle(name) {
+    var link = document.createElement("link");
+    // loading a style sheet from JS
+    // see http://stackoverflow.com/questions/574944/ddg#577002
+    link.id   = name;
+    link.rel  = 'stylesheet';
+    link.type = 'text/css';
+    link.href = botRoot + name;
+    link.media = 'all';
+    document.head.appendChild(link);
+    console.log("loadStyle", name);
+  };
+
   // load as described here:
   // https://developers.google.com/blockly/guides/configure/web/fixed-size
   loadScript("blockly/blockly_compressed.js");
@@ -19,5 +32,6 @@
   loadScript("blockly/msg/js/en.js");
   // load our own code
   loadScript("bot/workspace.js");
+  loadStyle("bot/workspace.css");
 })();
 
