312c2d28186912eb8f961f266e7bc95c141bead4
Nicco Kunzmann <niccokunzmann@rambler.ru>
2018-11-21 23:45:42 +0100

    Anleitung zum Laden geschrieben

---
+++ index.js
@@ -0,0 +1,12 @@
+
+function fillCode() {
+  var element = document.getElementById("source");
+  var location = "" + document.location;
+  element.innerText = "var botRoot = \"" + location + "\"; \nvar script = document.createElement(\"script\"); \nscript.src = location + \"load-bot.js\"; \ndocument.head.appendChild(script);";
+}
+
+window.addEventListener("load", fillCode)
+
+
+
+
